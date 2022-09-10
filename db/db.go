c90af935fd4004d7e539190f8ac8cd05668ee6b4
GenkiHirano <genki86web@gmail.com>
2022-09-10 20:10:07 +0900

    feat: GormでDB周りの処理実装

--- db/db.go
+++ db/db.go
@@ -1,1 +1,59 @@
 package db
+
+import (
+	"github.com/GenkiHirano/gqlgen-tutorial/graph/model"
+	"gopkg.in/ini.v1"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type ConfigList struct {
+	Host     string
+	Database string
+	User     string
+	Port     int
+	Password string
+}
+
+var DB *gorm.DB
+
+func LoadConfig() *ConfigList {
+	config, err := ini.Load("./db/dbconfig.ini")
+	if err != nil {
+		panic(err)
+	}
+
+	return &ConfigList{
+		Host:     config.Section("db").Key("host").String(),
+		Database: config.Section("db").Key("database").String(),
+		User:     config.Section("db").Key("user").String(),
+		Port:     config.Section("db").Key("host").MustInt(),
+		Password: config.Section("db").Key("password").String(),
+	}
+}
+
+func InitDB() {
+	config := LoadConfig()
+
+	dns := "user=" + config.User + " password=" + config.Password + " host=" + config.Host + " dbname=" + config.Database + " sslmode=disable"
+	gormDB, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	DB = gormDB
+	DB.AutoMigrate(&model.User{}, &model.Todo{})
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
+
+func CloseDB() {
+	database, err := DB.DB()
+	if err != nil {
+		panic(err)
+	}
+	database.Close()
+}
